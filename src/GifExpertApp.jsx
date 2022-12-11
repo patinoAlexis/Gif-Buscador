4178dc40ba3f680c3a1dc6ef62a63c659a6ff2ae
Alexis <AlexisIVVI@outlook.com>
2022-12-11 02:02:23 -0800

    Initial commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+
+export const GifExpertApp = () => {
+    return (
+        <>
+        {/* Titulo de la aplicacion */}
+            <h1>GifExpertApp</h1>
+            {/* Input */}
+
+            {/* Listado de gifs */}
+                {/* Gifs especificos */}
+        </>
+    )
+}
